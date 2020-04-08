7f1e5edede166c7e54e97a11700955cc127a684d
ionutdejeu <ionut.dejeu@gmail.com>
2020-04-08 08:42:38 +0300

    Working SignIn Handler for Gernerating valid jwt

--- handlers.go
+++ handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
@@ -26,7 +28,7 @@ type claims struct {
 }
 
 func main() {
-	http.HandleFunc("/signin", signinHandler)
+	http.HandleFunc("/signin", signInHandler)
 	http.HandleFunc("/welcome", welcomeHandler)
 	http.HandleFunc("/refresh", refreshHandler)
 
@@ -34,8 +36,52 @@ func main() {
 	checkError(err)
 }
 
-func signinHandler(w http.ResponseWriter, r *http.Request) {
+func signInHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Signin handler")
+	var cr creds
+	err := json.NewDecoder(r.Body).Decode(&cr)
+	fmt.Println("creds", err, cr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	expectedPass, ok := users[cr.Username]
+
+	// when the user exist then we check the passwords
+	// else we return 401 Unathorized HTTP
+	if !ok || expectedPass != cr.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	expirationTime := time.Now().Add(5 * time.Minute)
+	// Create the JWT claims, which includes the username and expiry time
+	cl := &claims{
+		Username: cr.Username,
+		StandardClaims: jwt.StandardClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, cl)
+	// Create the JWT string
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		// If there is an error in creating the JWT return an internal server error
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Finally, we set the client cookie for "token" as the JWT we just generated
+	// we also set an expiry time which is the same as the token itself
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+
 }
 func welcomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Welcome handler")
